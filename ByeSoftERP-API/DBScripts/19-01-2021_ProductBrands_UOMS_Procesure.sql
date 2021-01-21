aeb15a0a04049e41f86f2dc1ffea2ebb4e03b1e8
Punam-Dev <punam.pal.dev@gmail.com>
2021-01-21 13:11:08 +0530

    ProductBrands & UOMS Get Procedure and Insert Script in Table

---
+++ ByeSoftERP-API/DBScripts/19-01-2021_ProductBrands_UOMS_Procesure.sql
@@ -0,0 +1,15 @@
+Create Procedure sp_GetProductTypes
+As 
+Begin
+	Select * From ProductTypes;
+End
+
+Go
+
+Create Procedure sp_GetUOMs
+As 
+Begin
+	Select * From UOMS;
+End
+
+Go
---
+++ ByeSoftERP-API/DBScripts/21-01-2021_ProductBrands_UOMS_Insert_Script.sql
@@ -0,0 +1,20 @@
+--Select * From ProductTypes
+--Insert script of Product Types table
+Insert Into ProductTypes Values('6d6500cd-356d-4ff3-a87b-1bf22841ab91','Raw Material','Raw Material',1);
+Insert Into ProductTypes Values('5be19df3-9638-4b90-bc86-30a61f30bcd2','Finished Goods','Finished Goods',2);
+Insert Into ProductTypes Values('7b50d744-52ad-4443-855e-38ece165b754','Packing Material','Packing Material',3);
+Insert Into ProductTypes Values('ae500c61-3f27-45d5-bf95-47c813a38dfa','Packaging','Packaging',4);
+Insert Into ProductTypes Values('1d638b20-e7d9-4eee-b060-6f462f0231c9','Jobwork','Jobwork',5);
+Insert Into ProductTypes Values('9c2c6eb1-5f83-43de-b709-e2a61281a051','Semifinished','Semifinishedl',6);
+Insert Into ProductTypes Values('87940a76-8315-4a25-aa1a-9a4438ae660c','Accessory','Accessory',7);
+Insert Into ProductTypes Values('25193ad4-023c-4f0a-b860-0930623b3682','Trading','Trading',8);
+Insert Into ProductTypes Values('03582c11-b027-4720-bee4-f103061e882f','Stores and Consumables','Stores and Consumables',9);
+Insert Into ProductTypes Values('a3fdcaf2-0f98-4078-a15c-924726bf8bcd','Capital WIP','Capital WIP',10);
+Insert Into ProductTypes Values('e0ee0947-1a1f-4ea1-b281-c3554c2998a7','Service','Service',11);
+
+--Select * From UOMS
+--Insert Scripts of UOMS
+Insert Into UOMS Values('6d6500cd-356d-4ff3-a87b-1bf22841ab91','Pcs','Pcs','Pcs',1)
+Insert Into UOMS Values('87940a76-8315-4a25-aa1a-9a4438ae660c','Unit','Unit','Unit',2)
+Insert Into UOMS Values('a3fdcaf2-0f98-4078-a15c-924726bf8bcd','Kgs','Kgs','Kilogram',3)
+
